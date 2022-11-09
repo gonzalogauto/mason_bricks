f181421d708780573e533ee82376572fe920d361
Gonza <gonzalo.gauto@gmail.com>
2022-11-09 11:35:48 -0300

    adding hooks - now we run a post_gen hook to add crashlytics depencency.

---
+++ custom_logger/hooks/post_gen.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+import 'package:mason/mason.dart';
+
+void run(HookContext context) async {
+  final logger = context.logger;
+  final useCrashlytics = context.vars['use_crashlytics'];
+  if (!useCrashlytics) return;
+  final isUsingFvm = logger.confirm('Are you using fvm?', defaultValue: true);
+  await addCrashlyticsDependencie(logger, isUsingFvm: isUsingFvm);
+}
+
+Future<void> addCrashlyticsDependencie(
+  Logger logger, {
+  required bool isUsingFvm,
+}) async {
+  final using = isUsingFvm ? 'using fvm' : '';
+  final installCrashlytics = logger.progress('Adding crashlytics $using..');
+  final addCrashlyticsArgs = ['pub', 'add', 'firebase_crashlytics'];
+  if (isUsingFvm) await Process.run('fvm', ['flutter', ...addCrashlyticsArgs]);
+  if (!isUsingFvm) await Process.run('flutter', addCrashlyticsArgs);
+  installCrashlytics.complete('Crashlytics added!');
+}
